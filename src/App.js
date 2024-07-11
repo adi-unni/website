bd92208fc051cd994fd8f3af354c7b359897d036
Aditya_Unnithan_ONS <a_unnithan@hotmail.com>
2024-07-11 14:55:41 +0100

    first deploy

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import './App.css';
 
+function SuperButton() {
+  return(
+    <button>
+      Enter the matrix
+    </button>
+  )
+}
+
 function App() {
   return (
     <div className='flex flex-col items-center justify-center min-h-screen bg-gray-100'>
       <h1 className='text-4xl font-bold text-blue-500'> Aditya Unnithan </h1>
       <h2 className='text-2xl font-bold text-blue-500'> Full Stack Software Engineer </h2>
       <p className='text-lg text-blue-600'> Welcome to my website</p>
-      <p className='text-lg text-blue-600'> hi </p>
+      <SuperButton />
     </div>
   );
 }
